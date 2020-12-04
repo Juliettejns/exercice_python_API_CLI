7f3f8ca1e2ee73a93e56a0be7a62253956ba9fe9
Juliette <juliette.janes@yahoo.fr>
2020-12-04 15:52:59 +0100

    Ajout de la documentation de la Fonction, création de l'argument type afin de permettre à l'utilisateur de moduler sa requête et du paramètre de sortie resultat sous forme d'une liste

--- exercice_CLI.py
+++ exercice_CLI.py
@@ -7,12 +7,24 @@ sort un fichier CSV (?) contenant les données qui y sont associées.
 import requests, json
 
 # Création du programme de base avant transformation en CLI
-# Requetage JSon: récupération de l'url puis des données associées pour le cas de documentaryunit
+# Requetage JSon: récupération de l'url puis des données associées.
+# Pour l'instant j'ai uniquement travaillé sur un mot clé correspondant au type mais je pense qu'on peut tenter d'
+# ajouter un deuxième argument, par exemple ne faire ressortir que les objets où l'on retrouve ce 2ème argument?
 
-def documentaryUnitrecup(mot_cle):
+def requetage(mot_cle):
+    """Récupère les données correspondant à la requête faite par l'utilisateur sur l'API EHRI
+     et créé un fichier csv avec celles-ci
+
+    :param mot_cle: chaîne de caractères rentrée par l'utilisateur
+    :type mot_cle: str
+    :param resultat: liste de liste contenant les données importantes de la requête
+    :type resultat: list
+     """
+
     url = "https://portal.ehri-project.eu/api/v1/search?type="+mot_cle
     requete = requests.get(url)
     donnees = requete.json()
+    resultat=[]
     for objet in donnees['data']:
         #exemple de données à récupérer, à voir lesquelles prendre
         id=objet['id']
@@ -20,7 +32,8 @@ def documentaryUnitrecup(mot_cle):
         nom=objet['attributes']['descriptions'][0]['name']
         lien=objet['links']['self']
         #pour l'instant uniquement affichage des donnés obtenues, à mettre sous forme csv après?
-        print(id, type, nom, lien)
+        resultat.append([id, type, nom, lien])
+    print(resultat)
 
-recherche_utilisateur=input('Rentrez un mot clé (Documentary Unit):')
-documentaryUnitrecup(recherche_utilisateur)
+recherche_utilisateur=input('Rentrez un mot clé (DocumentaryUnit, HistoricalAgent, repository ou country):')
+requetage(recherche_utilisateur)
