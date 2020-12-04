b4c9a1e89158ae35602862dd4110fb5ec5ce77f3
Juliette <juliette.janes@yahoo.fr>
2020-12-04 15:26:27 +0100

    Création d'une premère fonction permettant le requêtage de l'API via un mot clé rentré par l'utilisateur, uniquement documentaryUnit pour l'instant

--- exercice_CLI.py
+++ exercice_CLI.py
@@ -4,3 +4,23 @@ Il prend en entrée une requête à base de mots clés et,à partir de l'API EHR
 sort un fichier CSV (?) contenant les données qui y sont associées.
 """
 
+import requests, json
+
+# Création du programme de base avant transformation en CLI
+# Requetage JSon: récupération de l'url puis des données associées pour le cas de documentaryunit
+
+def documentaryUnitrecup(mot_cle):
+    url = "https://portal.ehri-project.eu/api/v1/search?type="+mot_cle
+    requete = requests.get(url)
+    donnees = requete.json()
+    for objet in donnees['data']:
+        #exemple de données à récupérer, à voir lesquelles prendre
+        id=objet['id']
+        type=objet['type']
+        nom=objet['attributes']['descriptions'][0]['name']
+        lien=objet['links']['self']
+        #pour l'instant uniquement affichage des donnés obtenues, à mettre sous forme csv après?
+        print(id, type, nom, lien)
+
+recherche_utilisateur=input('Rentrez un mot clé (Documentary Unit):')
+documentaryUnitrecup(recherche_utilisateur)
