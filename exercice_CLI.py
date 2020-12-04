fbdf2a0cb70423eb5f874dea715c4519a1965f3f
Juliette <juliette.janes@yahoo.fr>
2020-12-04 14:22:30 +0100

    Création du ficher exercice_CLI.py et description du fonctionnement du programme

---
+++ exercice_CLI.py
@@ -0,0 +1,6 @@
+"""
+Programme réalisé dans le cadre d'un exercice Python CLI
+Il prend en entrée une requête à base de mots clés et,à partir de l'API EHR Portal,
+sort un fichier CSV (?) contenant les données qui y sont associées.
+"""
+
