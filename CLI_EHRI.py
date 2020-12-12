b4a7c8c98278164f3c7e729d0b8a216fe011c4bb
Juliette <juliette.janes@yahoo.fr>
2020-12-12 20:11:35 +0100

    Réorganisation des requêtes et de la sortie (nom et type)

--- CLI_EHRI.py
+++ CLI_EHRI.py
@@ -8,7 +8,7 @@ import requests, json, click
 
 
 
-def requetage(mot_cle):
+def requetage(q):
     """Fait une recherche sur l'API EHRI à partir de l'entrée d'un mot-clé issu de cette liste restreinte:
     Country, Repository, HistoricalAgent, DocumentaryUnit.
     :param mot_cle: chaîne de caractères rentrée par l'utilisateur.
@@ -16,25 +16,25 @@ def requetage(mot_cle):
     :param resultat: liste de liste contenant les données importantes de la requête
     :type resultat: list
     """
-
-    url = "https://portal.ehri-project.eu/api/v1/search?type=" + mot_cle
-    requete = requests.get(url)
+    parametres={"q":q}
+    url = "https://portal.ehri-project.eu/api/v1/search?"
+    requete = requests.get(url, params=parametres)
     donnees = requete.json()
     resultat=[]
     for objet in donnees['data']:
+
         try:
-            # exemple de données à récupérer: ici celles qui fonctionnent pour tout les mots_clé
+            # exemple de données à récupérer: ici celles qui fonctionnent pour tout les types de données documentaryUnt...
             # on essaie d'obtenir le nom si il existe, une description si elle existe, les fichiers n'étant pas identiques
             resultat.append({"id": objet['id'],
-                             "type": objet['type'],
-                             "lien": objet['links']['self'],
-                             "nom":[]
+                            "type": objet['type'],
+                            "lien": objet['links']['self'],
+                            "nom":[]
                              })
             if objet['type']=="Repository" or objet['type']=="HistoricalAgent":
                 resultat[-1]['nom'].append(objet['attributes']['name'])
             elif objet['type']=="DocumentaryUnit":
                 resultat[-1]['nom'].append(objet['attributes']['descriptions'][0]['name'])
-
         except (IndexError, KeyError):
             pass
     return resultat
@@ -48,7 +48,7 @@ def run(query, full):
     print("Nombre de résultats:{}".format(len(resultat)))
 
     for objet in resultat:
-        print("id:{}, nom:{}".format(objet['id'], objet['nom']))
+        print("nom:{}, type:{}".format(objet['nom'], objet['type']))
     if full:
         for objet in resultat:
             print("id: {}, nom:{}, type:{}, lien:{}".format(objet['id'], objet['nom'], objet['type'], objet['lien']))
